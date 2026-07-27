3e081e12072ff67ea3540af4c31101a597c14360
GabrielAlmeidaFlores <GabrielAlmeidaFlores@users.noreply.github.com>
2026-07-27 11:12:20 +0000

    deploy: 5733d3832c66c26072fa9db053543c7853e1060e

--- assets/architecture-TIHT7OUA-Cc-5G1l6.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-KEIR6QF5-Dj-OpFgW.js";import{M as e}from"./mermaid-parser.core-DbQDNAcs.js";export{e as createArchitectureServices};
--- assets/swimlanesDiagram-G3AALYLV-BQt8YUWL.js
+++
@@ -1,8 +0,0 @@
-import{n as e}from"./chunk-Y2CYZVJY-DsF7k-Jl.js";import"./src-Cz8bXOH0.js";import"./chunk-WYO6CB5R-Caf_vuqE.js";import"./chunk-ICXQ74PX-X2ro8oZp.js";import"./chunk-HOUHSVGY-nsuvbJdk.js";import"./chunk-Q4XR5HBZ-DDKpexCz.js";import"./chunk-7BUUIJ7U-Bb538aSH.js";import"./chunk-OGEWGWER-B83EOa5M.js";import"./chunk-32BRIVSS-Be6INlwJ.js";import"./chunk-XXDRQBXY-DlK_iNGL.js";import"./chunk-VR4S4FIN-BlpLugn8.js";import"./chunk-C7G6YPKG-PD3vMAI3.js";import"./chunk-ZGVPDNZ5-ByWjvuWI.js";import"./chunk-52WLFC77-D28jT92-.js";import"./chunk-FWX5IMBZ-lAh6MNeb.js";import"./chunk-ZIRB5QZD-C6fEPe3t.js";import{r as t,t as n}from"./chunk-PUDLZKDR-xBnl8xQ-.js";var r=n({defaultLayout:`swimlane`,styles:e(e=>`${t(e)}
-  .swimlane.cluster rect {
-    stroke: ${e.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,`getStyles`)});export{r as diagram};
